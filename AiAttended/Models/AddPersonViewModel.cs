c0ada199ab8eb62e7255167bae6c7c9f6083196b
Isreal Ulelu <isrealulelu@Advansios-MacBook-Pro.local>
2021-07-12 12:55:24 +0100

    Manager controller deleted

--- AiAttended/Models/AddPersonViewModel.cs
+++ AiAttended/Models/AddPersonViewModel.cs
@@ -1,11 +1,13 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 using Microsoft.AspNetCore.Http;
 
 namespace AiAttended.Models
 {
-    public class ImageModel
+    public class AddPersonViewModel
     {
         public string Name { get; set; }
+        [Required]
         public IFormFileCollection Images { get; set; }
     }
 }
--- AiAttended/Services/AzureService.cs
+++ AiAttended/Services/AzureService.cs
@@ -30,7 +30,7 @@ namespace AiAttended.Services
             _context = context;
         }
 
-        public async Task<ResponseManager> AddPersonAsync(ImageModel model)
+        public async Task<ResponseManager> AddPersonAsync(AddPersonViewModel model)
         {
             var existingUser = _context.People.Where(x => x.Name == model.Name);
             if(existingUser != null)
@@ -86,12 +86,6 @@ namespace AiAttended.Services
                     PersistedFace face = await faceClient.LargePersonGroupPerson.AddFaceFromStreamAsync(largePersonGroupId: personGroupId, personId: person.PersonId, image: fileStream);
                 }
             }
-            //var user = new User
-            //{
-            //    Id = person.PersonId,
-            //    Name = model.Name,
-            //    Created = DateTime.Now
-            //};
 
             _context.People.Add(person);
             var result = await _context.SaveChangesAsync();
@@ -120,6 +114,15 @@ namespace AiAttended.Services
         public async Task<ResponseManager> TrainGroupAsync()
         {
             var personGroupId = _configuration["AzureDetails:PersonGroupID"];
+            var personGroupExists = await GetPersonGroup();
+            if (!personGroupExists.isSuccess)
+            {
+                return new ResponseManager
+                {
+                    isSuccess = false,
+                    Message = "Unable to find person group. First, create a group by adding a person",
+                };
+            }
             await faceClient.LargePersonGroup.TrainAsync(personGroupId);
 
             // Wait until the training is completed.
@@ -150,16 +153,37 @@ namespace AiAttended.Services
 
 
 
-        public async Task<Tuple<ResponseManager, List<Tuple<Person, double>>>> IdentifyFacesAsync(ImageModel model)
+        public async Task<Tuple<ResponseManager, List<Tuple<Person, double>>>> IdentifyFacesAsync(AddPersonViewModel model)
         {
+            var response = new ResponseManager();
 
             var personGroupId = _configuration["AzureDetails:PersonGroupID"];
             var recognitionModel = _configuration["AzureDetails:RecognitionModel"];
             var identifiedPersons = new List<Tuple<Person, double>>();
 
             List<Guid> sourceFaceIds = new List<Guid>();
-            List<DetectedFace> detectedFaces = await DetectFaceRecognize(faceClient, model.Images, recognitionModel);
-            var response = new ResponseManager();
+
+            var personGroupExists = await GetPersonGroup();
+            if (!personGroupExists.isSuccess)
+            {
+                response = new ResponseManager
+                {
+                    isSuccess = false,
+                    Message = "Unable to find person group. First, create a group by adding a person",
+                };
+                return new Tuple<ResponseManager, List<Tuple<Person, double>>>(response, null);
+            }
+
+            List<DetectedFace> detectedFaces = await DetectFace(faceClient, model.Images, recognitionModel);
+            if(detectedFaces == null)
+            {
+                response = new ResponseManager
+                {
+                    isSuccess = false,
+                    Message = "No face detected in the image(s)",
+                };
+                return new Tuple<ResponseManager, List<Tuple<Person, double>>>(response, null);
+            }
 
             // Add detected faceId to sourceFaceIds.
             foreach (var detectedFace in detectedFaces)
@@ -232,7 +256,7 @@ namespace AiAttended.Services
                 return new ResponseManager
                 {
                     isSuccess = true,
-                    Message = response.Data.UserData,
+                    Message = response.Data.LargePersonGroupId.ToString(),
                 };
             }
             else
@@ -240,16 +264,16 @@ namespace AiAttended.Services
                 return new ResponseManager
                 {
                     isSuccess = false,
-                    Message = response.ErrorMessage,
+                    Message = response.StatusDescription,
                 };
             }
         }
 
 
-        public async Task<List<DetectedFace>> DetectFaceRecognize(IFaceClient faceClient, IFormFileCollection image, string recognition_model)
+        public async Task<List<DetectedFace>> DetectFace(IFaceClient faceClient, IFormFileCollection image, string recognition_model)
         {
             IList<DetectedFace> detectedFaces = null;
-            IList<DetectedFace> allDetectedFaces = null;
+            List<DetectedFace> allDetectedFaces = new List<DetectedFace>();
             // We use detection model 3 because we are not retrieving attributes.
             foreach (var similarImage in image)
             {
@@ -257,19 +281,25 @@ namespace AiAttended.Services
                 string fileName = Path.GetFileNameWithoutExtension(similarImage.FileName);
                 string extension = Path.GetExtension(similarImage.FileName);
                 fileName = fileName + DateTime.Now.ToString("yymmssfff") + extension;
-                string path = Path.Combine(wwwRootPath + "/img/", fileName);
-                using (var fileStream = new FileStream(path, FileMode.Create))
+                string path = Path.Combine(wwwRootPath + "/images/", fileName);
+                using (var fileStream = new FileStream(path, FileMode.Create, FileAccess.ReadWrite, FileShare.ReadWrite))
                 {
                     await similarImage.CopyToAsync(fileStream);
-                    detectedFaces = await faceClient.Face.DetectWithStreamAsync(image: fileStream, recognitionModel: recognition_model, detectionModel: DetectionModel.Detection03);
-                }
-
-                foreach(var face in detectedFaces)
-                {
-                    allDetectedFaces.Add(face);
+                    using (Stream imageStream = File.OpenRead(path))
+                    {
+                        detectedFaces = await faceClient.Face.DetectWithStreamAsync(imageStream, recognitionModel: recognition_model, detectionModel: DetectionModel.Detection03);
+                        if (detectedFaces.Count < 1)
+                            return null;
+                        foreach (var face in detectedFaces)
+                        {
+                            allDetectedFaces.Add(face);
+                        }
+                    }
                 }
             }
-            return allDetectedFaces.ToList();
+            if (allDetectedFaces.Count < 1)
+                return null;
+            return allDetectedFaces;
         }
 
 
